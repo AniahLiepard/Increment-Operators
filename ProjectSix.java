5edae3c462b816baca893fd6dc2e997ba1e23ec6
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 09:56:02 -0400

    Increment Operators Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17974

---
+++ ProjectSix.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ProjectSix {
+	public static void main(String args[]){
+		Scanner cupcakes = new Scanner(System.in);
+		
+		int SweetTarts = 27;
+		int Spree = 34;
+		
+		++SweetTarts;
+		System.out.println(SweetTarts);
+	}
+}
